116bf298a983b09303e3fe801fe490a12e90b50c
andrMiotto <andre_lm_pereira@estudante.sesisenai.org.br>
2026-03-12 21:18:05 -0300

    feat: adiciona-validacoes

--- gym-hub/src/main/java/com/spartans/dev/gym_hub/dto/exercicio/ExercicioRequisicaoDTO.java
+++ gym-hub/src/main/java/com/spartans/dev/gym_hub/dto/exercicio/ExercicioRequisicaoDTO.java
@@ -1,14 +1,30 @@
 package com.spartans.dev.gym_hub.dto.exercicio;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
 public record ExercicioRequisicaoDTO(
 
+        @NotBlank(message = "O nome do exercício é obrigatório")
         String nome,
-        String descricao,
-        String videoURL,
-        int series,
-        int repeticoes,
-        String musculoAlvo
 
+        @NotBlank(message = "A descrição não pode estar em branco")
+        String descricao,
+
+        @NotBlank(message = "A URL do vídeo de instrução é obrigatória")
+        String videoURL,
+
+        @Positive(message = "O número de séries deve ser maior que zero")
+        @Min(value = 1, message = "O mínimo de séries é 1")
+        int series,
+
+        @Positive(message = "O número de repetições deve ser maior que zero")
+        @Min(value = 1, message = "O mínimo de repetições é 1")
+        int repeticoes,
+
+        @NotBlank(message = "Informe o músculo alvo deste exercício")
+        String musculoAlvo
 
 
 ) {
--- gym-hub/src/main/java/com/spartans/dev/gym_hub/dto/professor/ProfessorRequisicaoDTO.java
+++ gym-hub/src/main/java/com/spartans/dev/gym_hub/dto/professor/ProfessorRequisicaoDTO.java
@@ -1,19 +1,31 @@
 package com.spartans.dev.gym_hub.dto.professor;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.*;
 
 public record ProfessorRequisicaoDTO(
-         String nome,
+        @NotBlank(message = "O nome do professor é obrigatório")
+        @Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres")
+        String nome,
 
-         String cref,
+        @NotBlank(message = "O CREF é obrigatório")
+        @Pattern(regexp = "\\d{5,10}-[GA]/[A-Z]{2}", message = "O formato do CREF deve ser 00000-G/SP")
+        String cref,
 
-         String especialidade,
+        @NotBlank(message = "A especialidade não pode estar em branco")
+        String especialidade,
 
-         String sobre,
+        @NotBlank(message = "O campo 'sobre' é necessário para o perfil")
+        @Size(max = 500, message = "O resumo deve ter no máximo 500 caracteres")
+        String sobre,
 
-         Double avaliacao,
+        @DecimalMin(value = "0.0")
+        @DecimalMax(value = "5.0", message = "A avaliação máxima é 5.0")
+        Double avaliacao,
 
-         String cpf
+        @NotBlank(message = "O CPF é obrigatório")
+        @Pattern(regexp = "\\d{11}", message = "O CPF deve conter apenas os 11 dígitos numéricos")
+        String cpf
 
 
 ) {
