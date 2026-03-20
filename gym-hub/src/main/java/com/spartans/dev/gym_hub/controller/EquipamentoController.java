4378e0487c8e556be6bf9adb23ab06e5ce5088ae
LEANDRO FILIPY DE LIMA <leandro_f_lima@estudante.sesisenai.org.br>
2026-03-20 20:47:12 -0300

    fix: remove os try catchs desnecessarios do controller

--- gym-hub/src/main/java/com/spartans/dev/gym_hub/controller/EquipamentoController.java
+++ gym-hub/src/main/java/com/spartans/dev/gym_hub/controller/EquipamentoController.java
@@ -19,48 +19,28 @@ public class EquipamentoController {
 
     @PostMapping("/register")
     public EquipamentoResponse create(@RequestBody EquipamentoRequest equipamentoRequest){
-        try{
             return iEquipamentoService.create(equipamentoRequest);
-        }catch (RuntimeException e){
-            throw new RuntimeException(e.getMessage());
-        }
     }
 
     @GetMapping("/list")
     public List<EquipamentoResponse> list (){
-        try{
             return iEquipamentoService.listAll();
-        }catch (RuntimeException e){
-            throw new RuntimeException(e.getMessage());
-        }
     }
 
     @GetMapping("/list/{id}")
     public EquipamentoResponse listById(@PathVariable long id){
-        try{
             return iEquipamentoService.findById(id);
-        }catch (RuntimeException e){
-            throw new RuntimeException(e.getMessage());
-        }
     }
 
     @PutMapping("update/{id}")
     public EquipamentoResponse update(@PathVariable long id, @RequestBody EquipamentoRequest equipamentoRequest){
-        try{
             return iEquipamentoService.update(id, equipamentoRequest);
-        }catch (RuntimeException e){
-            throw new RuntimeException(e.getMessage());
-        }
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity delete (@PathVariable long id){
-        try{
             iEquipamentoService.delete(id);
             return ResponseEntity.status(201).body("O equipamento foi deletado");
-        }catch (RuntimeException e){
-            throw new RuntimeException(e.getMessage());
-        }
     }
 
 }
